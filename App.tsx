a9de4563e5c0e0d6393e1baf488069ab7e529c96
Kacper Kałużny <kacper.kaluzny7@gmail.com>
2025-10-16 12:46:16 +0000

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
+import {
+  Provider as PaperProvider,
+  Button,
+  Card,
+  DefaultTheme,
+  MD3DarkTheme,
+} from "react-native-paper";
+import { useCounterStore } from "./store/useCounterStore";
 
 export default function App() {
+  const { count, increase, decrease, reset } = useCounterStore();
+
+  const [isDark, setIsDark] = React.useState(false);
+
+  const theme = isDark
+    ? {
+        ...MD3DarkTheme,
+        colors: {
+          ...MD3DarkTheme.colors,
+          primary: "tomato",
+          secondary: "yellow",
+        },
+      }
+    : {
+        ...DefaultTheme,
+        colors: {
+          ...DefaultTheme.colors,
+          primary: "tomato",
+          secondary: "yellow",
+        },
+      };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#f5f5f5",
+          padding: 16,
+        }}
+      >
+        <Card style={{ width: 300, padding: 16 }}>
+          <Card.Title title="Licznik Zustand" />
+          <Card.Content>
+            <Text
+              style={{ fontSize: 28, textAlign: "center", marginVertical: 10 }}
+            >
+              {count}
+            </Text>
+          </Card.Content>
+          <Card.Actions
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              marginTop: 10,
+            }}
+          >
+            <Button mode="contained" onPress={increase}>
+              +
+            </Button>
+            <Button mode="contained-tonal" onPress={decrease}>
+              -
+            </Button>
+            <Button mode="outlined" onPress={reset}>
+              Reset
+            </Button>
+          </Card.Actions>
+        </Card>
+        <View style={{ marginTop: 20 }}>
+          <Button mode="contained-tonal" onPress={() => setIsDark(!isDark)}>
+            {isDark ? "Tryb jasny" : "Tryb ciemny"}
+          </Button>
+        </View>
+      </View>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
