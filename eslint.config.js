10560e7b356c3198540362011ee835830a54b6f5
Kacper Kałużny <kacper.kaluzny7@gmail.com>
2025-10-17 06:31:06 +0000

    Add ESLing config

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    ignores: ["node_modules"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "no-unused-vars": "warn",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+  },
+];
